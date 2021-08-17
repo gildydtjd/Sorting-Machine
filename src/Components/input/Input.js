500bb5bd05ed52310240f718c146f9aba8c3dafb
blingblin-g <luvbliny@gmail.com>
2021-08-17 12:24:54 +0900

    refactor form tag

--- src/Components/input/Input.js
+++ src/Components/input/Input.js
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Input(props) {
   const [num, setNum] = useState([]);
-  const [pushNum, setPushNum] = useState('');
+  const [pushNum, setPushNum] = useState("");
 
   const numChange = (e) => {
     setPushNum(e.target.value);
   };
 
   const onReset = () => {
-    setPushNum('');
+    setPushNum("");
   };
 
-  const numClick = () => {
+  const numClick = (e) => {
+    e.preventDefault();
     setNum([...num, pushNum]);
     onReset();
   };
 
   const numRender = num.map((n, index) => {
     return (
-      <span key={index} style={{ border: '1px solid #c9c9c9', margin: '2px' }}>
+      <span key={index} style={{ border: "1px solid #c9c9c9", margin: "2px" }}>
         {n}
       </span>
     );
@@ -27,15 +28,16 @@ function Input(props) {
 
   return (
     <div>
-      <input type="text" onChange={numChange} value={pushNum}></input>
-      <button onClick={numClick}>클릭</button>
-
+      <form onSubmit={numClick}>
+        <input type="text" onChange={numChange} value={pushNum}></input>
+        <button>클릭</button>
+      </form>
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '200px',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "200px",
         }}
       >
         {numRender}
